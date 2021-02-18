455360d7e37e122f8b425e48843faeee00939bf9
Ilya <kaproman00@mail.ru>
2021-02-18 13:02:22 +0300

    feat: tracking config files

---
+++ utils/consts.js
@@ -0,0 +1,20 @@
+const ADCORE_URL = 'https://ad-core.ru/api.php';
+const ADCORE_KEY = 'ba6ab22fcde18bd0f5058eb3fe7ff996';
+const VKTARGET_URL = 'https://vktarget.ru/api/method/';
+const VKTARGET_KEY = '9373ea63f050e084acfa08056d7ad06a5d7e77dfd626213f8f2315a0940a041c';
+const SMMOK_URL = 'https://smmok-fb.ru/api/';
+const SMMOK_KEY = '72763#wku23DUrRA4jBqH9R4LJXYH9vTSF3uiAGmeUZnuMinU=#15ca82674482ba170aeb795f2b764523';
+const SPANEL_URL = 'https://socialpanel24.com/api/v2';
+const SPANEL_KEY = 'a6810f641f63a550137a52315d7f83ad';
+const VALUTE_URL = 'https://www.cbr-xml-daily.ru/daily_json.js';
+
+const SPREADSHEET_ID = '1af_ilY8FsygwpuZD4_onxkIs4BmtiWa2XDPgGT4AuMU'
+
+module.exports = {
+    ADCORE_KEY, ADCORE_URL,
+    VKTARGET_KEY, VKTARGET_URL,
+    SMMOK_KEY, SMMOK_URL,
+    SPANEL_KEY, SPANEL_URL,
+    VALUTE_URL,
+    SPREADSHEET_ID
+};
